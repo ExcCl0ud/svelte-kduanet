4ece8ffa1e8615fd88cbe984d03c06880b5c228b
ExcCl0ud <andiansyah0425@gmail.com>
2020-10-31 06:32:14 +0700

    2-HTML

---
+++ src/HowTo.svelte
@@ -0,0 +1,14 @@
+<style>
+    img {
+        max-width: 100%;
+    }
+</style>
+
+<div bp="grid">
+
+    <img 
+    bp="offset-5@md 4@md 12@sm" 
+    src="https://phptuts.github.io/fcc-handwashing/handwashing.gif" 
+    alt="How to wash your hands." />
+
+</div>
