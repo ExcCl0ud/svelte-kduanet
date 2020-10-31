ac9c7b4cd4b508cd1adf52c4e4d6281165480dd1
ExcCl0ud <andiansyah0425@gmail.com>
2020-10-31 08:53:32 +0700

    3-html

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,32 @@
 <script>
-	import HowTo from './HowTo.svelte';
-    import Timer from './Timer.svelte';
-</script>
-
-<style>
+    import Timer from "./Timer.svelte";
+    import HowTo from "./HowTo.svelte";
+    let audio;
+    function timerEnds(e) {
+      audio.play();
+    }
+  </script>
+  
+  <style>
     h1,
     h3 {
-        text-align: center;
+      text-align: center;
     }
-</style>
-
-<h1>Handwashing App</h1>
-
-<Timer />
-
-<HowTo />
-
-<h3>
-    <a href="https://www.who.int/gpsc/clean_hands_protection/en/">Picture Source</a>
-    <a href="https://freesound.org/people/metrostock99/sounds/345086/">Sound Source</a>
-</h3>
+  </style>
+  
+  <h1>Handwashing App</h1>
+  
+  <Timer on:end={timerEnds} />
+  
+  <HowTo />
+  
+  <h3>
+    <a href="https://www.who.int/gpsc/clean_hands_protection/en/">
+      Picture Source
+    </a>
+  
+    <a href="https://freesound.org/people/metrostock99/sounds/345086/ ">
+      Sound Source
+    </a>
+  </h3>
+  
--- src/Timer.svelte
+++ src/Timer.svelte
@@ -1,23 +1,53 @@
 <script>
-	import ProgessBar from './ProgessBar.svelte';
-</script>
-
-<style>
+    import { createEventDispatcher } from "svelte";
+    import ProgressBar from "ProgressBar.svelte";
+    const totalSeconds = 20;
+    let secondLeft = totalSeconds;
+    let isRunning = false;
+    $: progress = ((totalSeconds - secondLeft) / totalSeconds) * 100;
+    const dispatch = createEventDispatcher();
+    function startTimer() {
+      isRunning = true;
+      const timer = setInterval(() => {
+        secondLeft -= 1;
+        if (secondLeft == 0) {
+          clearInterval(timer);
+          isRunning = false;
+          secondLeft = totalSeconds;
+          dispatch("end");
+        }
+      }, 1000);
+    }
+  </script>
+  
+  <style>
     h2 {
-        margin: 0;
+      margin: 0;
     }
     .start {
-        background-color: rgb(154, 73, 73);
-        width: 100%;
-        margin: 10px 0;
+      background-color: rgb(154, 73, 73);
+      width: 100%;
+      margin: 10px 0;
     }
-</style>
-
-<div bp="grid">
-    <h2 bp="offset-5@md 4@md 12@sm" >Seconds Left:
-
-    </h2>
-</div>
-<ProgessBar />
-
-<button class="start">Start</button>
+    .start[disabled] {
+      background-color: rgb(194, 194, 194);
+      cursor: not-allowed;
+    }
+  </style>
+  
+  <div bp="grid">
+    <h2 bp="offset-5@md 4@md 12@sm">Seconds Left: {secondLeft}</h2>
+  </div>
+  <ProgressBar {progress} />
+  
+  <div bp="grid">
+  
+    <button
+      disabled={isRunning}
+      on:click={startTimer}
+      bp="offset-5@md 4@md 12@sm"
+      class="start">
+      Start
+    </button>
+  
+  </div>
